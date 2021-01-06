d9272aea128e3d8b2945b38e647cb5c3d9e7e0c5
Jahanzaib-Mirza <76735840+Jahanzaib-Mirza@users.noreply.github.com>
2021-01-06 15:59:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+var fullName= document.getElementById("name").value;
+var DOB= document.getElementById("dob").value;
+var email= document.getElementById("email").value;
+var contactNo=document.getElementById("contactNo").value;
+var address=document.getElementById("address").value;
+var genderMale=document.getElementById("male").value;
+var genderFemale=document.getElementById("female").value;
+
+function getInfo() {
+console.log(fullName);
+console.log(DOB);
+console.log(email);
+console.log(contactNo);
+console.log(address);
+
+
+    
+}
